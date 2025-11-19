031a0e1b2a66b615c88b0cb49feec7706b0d6dba
zihinbender <73230187+zihinbender@users.noreply.github.com>
2025-11-19 09:36:17 +0000

    Chapter 6 completed

---
+++ ch06-blog/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import post_list, post_detail
+
+urlpatterns = [
+    path("", post_list, name="home"),
+    path("post/<int:pk>/", post_detail, name="post_detail")
+]
