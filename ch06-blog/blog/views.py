cf681c72ca0578a3aa4bb30aeb1f9247e76f6856
zihinbender <73230187+zihinbender@users.noreply.github.com>
2025-11-20 11:11:50 +0000

    Ch07 Forms - implemented

--- ch06-blog/blog/views.py
+++ ch06-blog/blog/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render, get_object_or_404
+#from django.shortcuts import render, get_object_or_404
+#from .models import Post
+#
+#def post_list(request):
+#    posts = Post.objects.all()
+#    return render(request, "home.html", {'posts': posts})
+#
+#def post_detail(request, pk):
+#    post = get_object_or_404(Post, pk=pk)
+#    return render(request, "post_detail.html", {"post": post})
+
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "home.html", {'posts': posts})
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, "post_detail.html", {"post": post})
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
 
