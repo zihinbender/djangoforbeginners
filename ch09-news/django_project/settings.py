05ff206078b1d404cad5488e2be033abf377b838
zihinbender <73230187+zihinbender@users.noreply.github.com>
2025-11-25 11:11:59 +0000

    custom user model

--- ch09-news/django_project/settings.py
+++ ch09-news/django_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+    "accounts",
+
+    # Default Apps.
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -121,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
